658621968ee1ea645f4a8252c7abfc9e49bf2876
SooTak <sootak@outlook.kr>
2025-03-04 17:00:13 +0900

    테스트4

---
+++ 깃 테스트2/Github test2.c
@@ -0,0 +1,12 @@
+/*
+	프로그램 내용 : 깃허브 연동 테스트2
+	날짜 : 2025.03.05(1주차)
+	이름 : 202311388 김민상
+*/
+#include <stdio.h>
+int main(void)
+{
+	int a = 1, b = 4;
+	printf("%d + %d = %d", a, b, a + b);
+	return 0;
+}
